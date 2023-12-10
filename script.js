b6485ffc50ec8da3b10c621c05cb2df9874b14e8
Muhammad Faisal <digital.1faisal@gmail.com>
2023-12-10 21:51:09 +0500

    commiot

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let startBtn = document.querySelector("#start-btn");
 let resetBtn = document.querySelector("#reset-btn");
 let restartBtn = document.querySelector("#restart-btn");
 let boxes = document.querySelectorAll(".box");
+let turnArrow = document.querySelector(".turn-arrow");
 let winner = document.getElementById("winner");
 let player1 = document.querySelector(".player-x h3");
 let player2 = document.querySelector(".player-o h3");
@@ -14,10 +15,12 @@ let turnO = true;
 startBtn.addEventListener("click", () => {
   gameBox.style.display = "none";
   startBtn.style.animation = "none";
+  turnArrow.innerHTML="<span>&#8592;</span>"
 });
 
 resetBtn.addEventListener("click", () => {
   resetGame();
+  turnArrow.innerHTML="<span>&#8592;</span>"
 });
 
 restartBtn.addEventListener("click", () => {
@@ -60,10 +63,14 @@ boxes.forEach((box) => {
     if (turnO) {
       box.innerHTML = "X";
       box.style.color = "darkViolet";
+      // turnArrow.style.transform = "rotateY(180deg)";
+      turnArrow.innerHTML="<span>&#8594;</span>"
       turnO = false;
     } else {
       box.innerHTML = "O";
-      box.style.color = "#138296";
+      box.style.color = "darkCyan";
+      turnArrow.innerHTML="<span>&#8592;</span>"
+      // turnArrow.style.transform = "";
       turnO = true;
     }
     box.disabled = "true";
@@ -71,34 +78,6 @@ boxes.forEach((box) => {
     checkWinner();
   });
 });
-
-// const checkWinner = () => {
-//   for (let pattern of winPatterns) {
-//     let box0val = boxes[pattern[0]].innerHTML;
-//     let box1val = boxes[pattern[1]].innerHTML;
-//     let box2val = boxes[pattern[2]].innerHTML;
-
-//     if (box0val !== "" && box0val !== "" && box0val !== "") {
-//       if (box0val === box1val && box1val === box2val) {
-//         for (let i = 0; i < 3; i++) {
-//           boxes[pattern[i]].style.backgroundColor = "#1EB8D9";
-//         }
-
-//         if (turnO) {
-//           winner.innerHTML = "Player 2<br>Win";
-//           player2wins++;
-//           player2.innerHTML = `Win ${player2wins}`;
-//         } else {
-//           winner.innerHTML = "Player 1<br>Win";
-//           player1wins++;
-//           player1.innerHTML = `Win ${player1wins}`;
-//         }
-//         winner.style.transform = "scale(150) translateY(-20%)";
-//         disableBoxes();
-//       }
-//     }
-//   }
-// };
 
 const checkWinner = () => {
   for (let pattern of winPatterns) {
@@ -121,7 +100,7 @@ const checkWinner = () => {
           player1wins++;
           player1.innerHTML = `Win ${player1wins}`;
         }
-        winner.style.transform = "scale(150) translateY(-20%)";
+        winner.style.transform = "scale(100) translateY(-20%)";
         disableBoxes();
         return 0;
       }
@@ -138,8 +117,8 @@ const checkWinner = () => {
   }
 
   if (isDraw) {
-    winner.innerHTML = "It's a Draw";
-    winner.style.transform = "scale(150) translateY(-20%)";
+    winner.innerHTML = "Match Draw";
+    winner.style.transform = "scale(100) translateY(-20%)";
     disableBoxes();
   }
 };
