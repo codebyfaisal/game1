3b4b8692826d949055f012e660113bcb6ff82be3
Muhammad Faisal <digital.1faisal@gmail.com>
2023-12-15 22:04:44 +0500

    commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let gameBox = document.querySelector(".game-starter");
+let gameStarter = document.querySelector(".game-starter");
 let startBtn = document.querySelector("#start-btn");
 let resetBtn = document.querySelector("#reset-btn");
 let restartBtn = document.querySelector("#restart-btn");
@@ -13,24 +13,31 @@ let player2wins = 0;
 let turnO = true;
 
 startBtn.addEventListener("click", () => {
-  gameBox.style.display = "none";
+  gameStarter.style.display = "none";
   startBtn.style.animation = "none";
-  turnArrow.innerHTML="<span>&#8592;</span>"
+  turnArrow.innerHTML = "<span>&#8592;</span>";
 });
 
 resetBtn.addEventListener("click", () => {
   resetGame();
-  turnArrow.innerHTML="<span>&#8592;</span>"
+  turnArrow.innerHTML = "<span>&#8592;</span>";
+  winner.style.fontSize = "0.001vMin";
 });
 
 restartBtn.addEventListener("click", () => {
   window.location.reload();
+  winner.style.fontSize = "0.001vMin";
+  winner.style.transform = "scale(0)";
 });
 
 const resetGame = () => {
   turnO = true;
   enableBoxes();
   winner.style.transform = "";
+};
+
+const screenWidth = () => {
+  return window.innerWidth;
 };
 
 const winPatterns = [
@@ -63,14 +70,14 @@ boxes.forEach((box) => {
     if (turnO) {
       box.innerHTML = "X";
       box.style.color = "darkViolet";
-      // turnArrow.style.transform = "rotateY(180deg)";
-      turnArrow.innerHTML="<span>&#8594;</span>"
+      box.style.textShadow = "0 4px 20px darkViolet";
+      turnArrow.innerHTML = "<span>&#8594;</span>";
       turnO = false;
     } else {
       box.innerHTML = "O";
       box.style.color = "darkCyan";
-      turnArrow.innerHTML="<span>&#8592;</span>"
-      // turnArrow.style.transform = "";
+      box.style.textShadow = "0 4px 20px darkCyan";
+      turnArrow.innerHTML = "<span>&#8592;</span>";
       turnO = true;
     }
     box.disabled = "true";
@@ -88,19 +95,22 @@ const checkWinner = () => {
     if (box0val !== "" && box0val !== "" && box0val !== "") {
       if (box0val === box1val && box1val === box2val) {
         for (let i = 0; i < 3; i++) {
-          boxes[pattern[i]].style.backgroundColor = "#1EB8D9";
+          boxes[pattern[i]].style.backgroundColor = "#29343a";
         }
 
         if (turnO) {
-          winner.innerHTML = "Player 2<br>Win";
+          winner.innerHTML = "Player 2 Win";
           player2wins++;
           player2.innerHTML = `Win ${player2wins}`;
         } else {
-          winner.innerHTML = "Player 1<br>Win";
+          winner.innerHTML = "Player 1 Win";
           player1wins++;
           player1.innerHTML = `Win ${player1wins}`;
         }
-        winner.style.transform = "scale(100) translateY(-20%)";
+
+        winner.style.fontSize = "11vMin";
+        winner.style.transform = "scale(1)";
+
         disableBoxes();
         return 0;
       }
@@ -118,7 +128,8 @@ const checkWinner = () => {
 
   if (isDraw) {
     winner.innerHTML = "Match Draw";
-    winner.style.transform = "scale(100) translateY(-20%)";
+    winner.style.fontSize = "11vMin";
+    winner.style.transform = "scale(1)";
     disableBoxes();
   }
 };
